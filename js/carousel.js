b2d3c393edca03efa917c745234dcafcb0613e0c
JR <jovincent.ruiz@gmail.com>
2017-07-03 16:54:17 -0700

    laid down html

---
+++ js/carousel.js
@@ -0,0 +1,12 @@
+//jquery controls for the carosel
+
+//goes to previous slide
+$("#previous").click(function() {
+	$(".carousel").carousel('prev');
+});
+
+
+//goes to next slide
+$("#next").click(function() {
+	$(".carousel").carousel('next');
+});
